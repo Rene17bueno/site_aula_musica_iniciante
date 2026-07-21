07f7f4237c96fcc55f64158b7429b15d2cc7d774
Rene17bueno <renebueno17@outlook.com>
2026-07-21 08:57:08 -0300

    chore: configura credenciais Firebase web app

--- js/js/community/firebase-config.js
+++ js/js/community/firebase-config.js
@@ -1,10 +1,10 @@
 // Preencha os valores reais no Firebase Console:
 // Project settings > General > Your apps > Web app > firebaseConfig
 export const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_PROJETO.firebaseapp.com",
-    projectId: "SEU_PROJECT_ID",
-    storageBucket: "SEU_PROJECT_ID.appspot.com",
-    messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-    appId: "SEU_APP_ID"
+    apiKey: "AIzaSyBqmUpwhk04fCfVhub_Lbv-MnkORd5QusA",
+    authDomain: "site-aula-musica-iniciante.firebaseapp.com",
+    projectId: "site-aula-musica-iniciante",
+    storageBucket: "site-aula-musica-iniciante.firebasestorage.app",
+    messagingSenderId: "971744554620",
+    appId: "1:971744554620:web:fc7fa5051452adb8ba23f9"
 };
